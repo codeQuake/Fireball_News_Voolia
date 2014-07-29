c0c05c9b8f37521f77019c6cd71e8d2d526cec29
Jens Krumsieck <info@codequake.de>
2014-07-29 13:41:05 +0200

    created content type based on fireball cms news type

---
+++ files/lib/system/content/type/VooliaNewsContentType.class.php
@@ -0,0 +1,52 @@
+<?php
+namespace cms\system\content\type;
+
+use news\data\category\NewsCategory;
+use news\data\category\NewsCategoryNodeTree;
+use news\data\news\NewsCategoryList;
+use cms\data\content\Content;
+use wcf\system\exception\UserInputException;
+use wcf\system\WCF;
+
+/**
+ *
+ * @author Jens Krumsieck
+ * @copyright codeQuake 2014
+ * @package de.codequake.cms
+ * @license GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl-3.0.txt>
+ */
+class NewsContentType extends AbstractContentType {
+
+	protected $icon = 'icon-archive';
+
+	public $objectType = 'de.codequake.cms.content.type.voolia.news';
+
+	public function validate($data) {
+		if (empty($data['categoryIDs'])) throw new UserInputException('categoryIDs', 'empty');
+	}
+
+	public function getFormTemplate() {
+		$excludedCategoryIDs = array_diff(NewsCategory::getAccessibleCategoryIDs(), NewsCategory::getAccessibleCategoryIDs(array(
+			'canAddNews'
+		)));
+		$categoryTree = new NewsCategoryNodeTree('de.codequake.cms.category.news', 0, false, $excludedCategoryIDs);
+		$categoryList = $categoryTree->getIterator();
+		$categoryList->setMaxDepth(0);
+		WCF::getTPL()->assign('categoryList', $categoryList);
+		return 'vooliaNewsContentType';
+	}
+
+	public function getOutput(Content $content) {
+		$data = $content->handleContentData();
+		$type = isset($data['type']) ? $data['type'] : 'standard';
+		$list = new NewsCategoryList($data['categoryIDs']);
+		$list->sqlLimit = $data['limit'];
+		$list->readObjects();
+		$list = $list->getObjects();
+		WCF::getTPL()->assign(array(
+			'objects' => $list,
+			'type' => $type
+		));
+		return WCF::getTPL()->fetch('vooliaNewsContentTypeOutput', 'cms');
+	}
+}
